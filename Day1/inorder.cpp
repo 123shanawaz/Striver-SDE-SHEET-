f63edc096ae030529ce6f2f83face2049b6ead0e
MD SHANAWAZ <91983494+123shanawaz@users.noreply.github.com>
2022-06-10 14:47:27 +0530

    Update inorder.cpp

--- Day1/inorder.cpp
+++ Day1/inorder.cpp
@@ -1,4 +1,26 @@
+/....DFS approch ...../
 
+
+
+
+class Solution {
+public:
+    void inorder(TreeNode* root,vector<int>&ans){
+        if(root==NULL)return ;
+        inorder(root->left,ans);
+        ans.push_back(root->val);
+        inorder(root->right,ans);
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        inorder(root,ans);
+        return ans;
+        
+    }
+};
+
+
+/.....BFS approch........ /
 
 
 class Solution {
